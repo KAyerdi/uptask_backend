d31525a23beff16905de096186dd0637ef4f96fb
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-09-26 16:46:10 -0300

    actualizando

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,11 +5,11 @@ import { exit } from 'node:process';
 
 export const connectDB = async () => {
   try{
-    const connection = await mongoose.connect(process.env.DATABASE_URL)
-    const url = `${connection.connection.host}:${connection.connection.port}`
+    const { connection } = await mongoose.connect(process.env.DATABASE_URL)
+    const url = `${connection.host}:${connection.port}`
     console.log(colors.magenta.bold(`MongoDB Conectado en: ${url}`))
   }catch(error){
-    console.log(error.message)
+    console.log(colors.red.bold('Error al conectar a MongoDB'))
     exit(1)
   }
 }
