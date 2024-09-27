2d1a0b2406b244b830fda051e5ebed3c20717d18
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-09-27 16:36:32 -0300

    Actualizando dependencias

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express"
 import Project from "../models/Project"
 
 export class ProjectController {
+
   static createProject = async (req: Request, res: Response) => {
     const project = new Project(req.body)
 
@@ -14,7 +15,28 @@ export class ProjectController {
   }
 
   static getAllProjects = async (req: Request, res: Response) => {
-    res.send('Todos los proyectos')
+    try{
+      const projects = await Project.find({})
+      res.json(projects)
+    }catch(error){
+      console.log(error)
+    }
 }
+
+static getProjectById = async (req: Request, res: Response) => {
+  const { id } = req.params
+  console.log(id)
+  try{
+    const project = await Project.findById(id)
+    if(!project){
+      const error = new Error('Proyecto no encontado')
+      return res.status(404).json({error: error.message})
+    }
+    res.json(project)
+  }catch(error){
+    console.log(error)
+  }
+}
+
 }
 
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,30 @@
 import { Router } from 'express'
+import { body, param } from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/',
+  body('projectName')
+  .notEmpty().withMessage('El nombre del Proyecto es Obligatorio'),
+
+  body('clientName')
+  .notEmpty().withMessage('El nombre del Cliente es Obligatorio'),
+
+  body('description')
+  .notEmpty().withMessage('La descripcion es Obligatoria'),
+
+  handleInputErrors,
+  
+  ProjectController.createProject
+)
+
 router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  ProjectController.getProjectById)
 
 export default router
