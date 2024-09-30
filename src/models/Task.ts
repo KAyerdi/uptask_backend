b5e11bef850e8cc592e08b585dfe6e63c8d488bf
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-09-30 00:04:26 -0300

    actualizando schema

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,5 +1,23 @@
 import mongoose, {Schema, Document} from "mongoose"
 
-export interface ProjectType extends Document {
-
+export interface ITask extends Document {
+  name: string,
+  description: string,
+  
 }
+
+export const TaskSchema : Schema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  }
+}
+)
+const Task = mongoose.model<ITask>('Task', TaskSchema)
+export default Task
