b80b839ca167c477aa6174d44502958daea9a593
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-09-30 00:54:08 -0300

    fix controller

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,8 +1,20 @@
-import mongoose, {Schema, Document} from "mongoose"
+import mongoose, {Schema, Document, Types} from "mongoose"
+
+const taskStatus = {
+  PENDING: 'pending',
+  ON_HOLD: 'onHold',
+  IN_PROGRESS: 'inProgress',
+  UNDER_REVIEW: 'underReview',
+  COMPLETED: 'completed'
+} as const
+
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
 
 export interface ITask extends Document {
-  name: string,
-  description: string,
+  name: string
+  description: string
+  project: Types.ObjectId
+  status: TaskStatus
   
 }
 
@@ -16,8 +28,17 @@ export const TaskSchema : Schema = new Schema({
     type: String,
     trim: true,
     required: true
+  },
+  project: {
+    type: Types.ObjectId,
+    ref: 'Project'
+  },
+  status: {
+    type: String,
+    enum: Object.values(taskStatus),
+    default: taskStatus.PENDING
   }
-}
+}, {timestamps: true}
 )
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 export default Task
