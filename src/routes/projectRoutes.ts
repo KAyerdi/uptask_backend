9df6757ab3ecb49fc1f9f0434f070942193293a2
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-10-01 11:31:37 -0300

    Actualizando task

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -3,6 +3,7 @@ import { body, param } from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
 import { handleInputErrors } from '../middleware/validation'
 import { TaskController } from '../controllers/TaskController'
+import { validateProjectExist } from '../middleware/project'
 
 const router = Router()
 
@@ -51,6 +52,7 @@ router.delete('/:id',
 )
 /** Routes for task */
 router.post('/:projectId/tasks',
+  validateProjectExist,
   TaskController.createTask
 )
 
